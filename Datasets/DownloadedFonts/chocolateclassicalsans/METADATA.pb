f780ab27f366a25162076a85bccb7e091b40b423
Abinav <ramasamyabinav@gmail.com>
2025-09-13 18:07:59 -0400

    Update requirements.txt, updated dataset, inital version of train.py

---
+++ Datasets/DownloadedFonts/chocolateclassicalsans/METADATA.pb
@@ -0,0 +1,37 @@
+name: "Chocolate Classical Sans"
+designer: "Moonlit Owen"
+license: "OFL"
+category: "SANS_SERIF"
+date_added: "2024-05-14"
+fonts {
+  name: "Chocolate Classical Sans"
+  style: "normal"
+  weight: 400
+  filename: "ChocolateClassicalSans-Regular.ttf"
+  post_script_name: "ChocolateClassicalSans-Regular"
+  full_name: "Chocolate Classical Sans Regular"
+  copyright: "Copyright 2024 The Chocolate Classical Sans Project Authors (https://github.com/MoonlitOwen/ChocolateSans)"
+}
+subsets: "chinese-traditional"
+subsets: "cyrillic"
+subsets: "latin"
+subsets: "latin-ext"
+subsets: "menu"
+subsets: "vietnamese"
+source {
+  repository_url: "https://github.com/MoonlitOwen/ChocolateSans"
+  commit: "0446a76d969f9788b0b339a9923f6541699df903"
+  config_yaml: "sources/project.yaml"
+  files {
+    source_file: "license.md"
+    dest_file: "OFL.txt"
+  }
+  files {
+    source_file: "fonts/ttf/ChocolateClassicalSans-Regular.ttf"
+    dest_file: "ChocolateClassicalSans-Regular.ttf"
+  }
+  branch: "main"
+}
+primary_script: "Hant"
+primary_language: "yue_Hant"
+stroke: "SANS_SERIF"
---
+++ train.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from classifier_CNN import BinaryCNN
+from Dataset import BinaryFontDataset
+
+if __name__ == "__main__":
+    # Initialize model
+    model = BinaryCNN()
+
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    model.to(device)
+
+
+    # Load dataset
+    train_dataset = BinaryFontDataset(
+    emnist_image_file="Datasets/EMNIST/emnist_source_files/emnist-byclass-train-images-idx3-ubyte",
+    font_ubyte_folder="Datasets/GoogleFonts-ubytes/Train"
+    )
+
+    # Loss and optimizer
+    criterion = nn.BCELoss()  # Binary Cross Entropy
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    num_epochs = 25
+    model.train()
+
+    # Create DataLoader
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device).unsqueeze(1)  # shape [batch,1]
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        avg_loss = running_loss / len(train_loader)
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}")
+    
+    
+    model.eval()  # set model to evaluation mode
+    all_labels = []
+    all_preds = []
+
+    test_dataset = BinaryFontDataset(
+    emnist_image_file="Datasets/EMNIST/emnist_source_files/emnist-byclass-test-images-idx3-ubyte",
+    font_ubyte_folder="Datasets/GoogleFonts-ubytes/Test"
+    )
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    with torch.no_grad():  # disable gradients for faster evaluation
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device).unsqueeze(1)
+            outputs = model(images)
+            preds = (outputs >= 0.5).float()  # convert probabilities to 0 or 1
+            all_labels.append(labels.cpu())
+            all_preds.append(preds.cpu())
+
+    all_labels = torch.cat(all_labels)
+    all_preds = torch.cat(all_preds)
+
+    accuracy = (all_preds == all_labels).float().mean()
+    print(f"Test Accuracy: {accuracy:.4f}")
